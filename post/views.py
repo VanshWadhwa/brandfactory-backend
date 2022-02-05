44c7d47acc150d3a33cd4eb2688bf601528947de
Vansh Wadhwa <vanshwadhwasphere@gmail.com>
2022-02-05 22:34:29 +0530

    added 4 new templates

--- post/views.py
+++ post/views.py
@@ -206,7 +206,138 @@ class PostView(APIView):
 
             print("6")
             return Response(res, status=status.HTTP_200_OK ,  content_type="image/jpeg")
-        elif temp == "temp3":
-            pass
+        elif temp[0] == "temp3":
+            # template 3 selected
+            IH = ImageHandler("tempImage3.png")
+
+            IH.resizeImage(x=1080, y=1080)
+
+            if (imageFrom[0] == "upload"):
+                savedFilename = imageLocation[0].name
+                filename = imageLocation[0].name
+                IH2 = ImageHandler(imageLocation[0] , primaryColor=prof.primaryColor , secondaryColor= prof.secondaryColor)
+
+            elif(imageFrom[0] == "url"):
+                filename = imageURL[0].split('/')[-1].split('.')[0] + ".png"
+    
+                savedFilename = filename
+
+                ID.downloadImage(filename, imageURL[0])
+        
+                IH2 = ImageHandler( str(filename))
+
+            else:
+                print('Invalid image upload type')
+            
+            IH2.resizeImage(x = 917 , y = 502 , type = "centre")
+            IH2.saveImage(filename, path="\\assets\\images\\")
+            del IH2
+            IH.overLayImage("\\output\\"+filename , atX = 79  , atY = 178)
+            importantWords = IH.addText(title[0] ,fontSize=5  , fontPath = "assets/fonts/Merriweather-Bold.ttf", atY = -200  , containsImportantWords = True , returnImportantWords = True )
+          
+
+            finalPath = IH.saveImage(filename)
+            del IH
+            location = '/output/' + savedFilename
+            print("4")
+
+            short_report = open(finalPath, 'rb')
+            report_encoded = base64.b64encode(short_report.read())
+
+            res =     {'status':'200' ,"msg" : "Post created", 'importantWords' : importantWords , 'imgLocation' : location , 'report': report_encoded , 'savedFileName' : savedFilename }
+            
+            short_report.close()
+            
+            os.remove(finalPath)
+            
+            print("5")
+
+            try:
+                if (imageFrom[0] == "url"):
+                    os.remove(str(pathlib.Path('.').cwd()) + "\\assets\\images\\" + savedFilename)
+                    print("removed")
+            except Exception as e :
+                print(e)
+
+            print("6")
+            return Response(res, status=status.HTTP_200_OK ,  content_type="image/jpeg")
+        elif temp[0] == "temp4":
+            # template 3 selected
+            filename = "content.png"
+            IH = ImageHandler("tempImage4.png")
+            importantWords = IH.addText(content[0] ,fontSize=4 , fontPath = "assets/fonts/Merriweather-Bold.ttf"    , marginX = 30  , atY = 200    , containsImportantWords=isContainImportantWords[0] , returnImportantWords=isContainImportantWords[0])
+
+            finalPath = IH.saveImage(filename)
+            # print("Saved as desc " , fileName +  "-desc.png")
+            
+            del IH
+            location = '/output/' + savedFilename
+            print("4")
+
+            short_report = open(finalPath, 'rb')
+            report_encoded = base64.b64encode(short_report.read())
+
+            res =     {'status':'200' ,"msg" : "Post created", 'importantWords' : importantWords , 'imgLocation' : location , 'report': report_encoded , 'savedFileName' : savedFilename }
+            
+            short_report.close()
+            
+            os.remove(finalPath)
+
+            print("5")
+
+            try:
+                if (imageFrom[0] == "url"):
+                    os.remove(str(pathlib.Path('.').cwd()) + "\\assets\\images\\" + savedFilename)
+                    print("removed")
+            except Exception as e :
+                print(e)
+
+            print("6")
+            return Response(res, status=status.HTTP_200_OK ,  content_type="image/jpeg")
+        elif temp[0] == "temp5":
+
+            IH = ImageHandler("qrcodeTemp.png")
+
+            filenameToBeSavedAs = "qrcode.png"
+            IH.createQRcode(title[0], ".\\assets\\images\\"+filenameToBeSavedAs)
+            IH2 = ImageHandler(filenameToBeSavedAs)
+            # IH2.resizeImage(x = 668 ,y = 668)
+            IH2.resizeImage(x = 1080 ,y =1080)
+            IH2.saveImage(path = "\\assets\\mages\\" , fileName =   filenameToBeSavedAs)
+            del IH2
+            IH.overLayImage("\\assets\\images\\"+filenameToBeSavedAs , atX = 206 , atY = 268)
+            finalPath = IH.saveImage(filenameToBeSavedAs)
+
+            del IH
+            location = '/output/' + savedFilename
+            print("4")
+
+            short_report = open(finalPath, 'rb')
+            report_encoded = base64.b64encode(short_report.read())
+
+            res =     {'status':'200' ,"msg" : "Post created",  'imgLocation' : location , 'report': report_encoded , 'savedFileName' : savedFilename }
+            
+            short_report.close()
+            
+            os.remove(finalPath)
+            
+            print("5")
+
+            try:
+                if (imageFrom[0] == "url"):
+                    os.remove(str(pathlib.Path('.').cwd()) + "\\assets\\images\\" + savedFilename)
+                    print("removed")
+            except Exception as e :
+                print(e)
+
+            print("6")
+            return Response(res, status=status.HTTP_200_OK ,  content_type="image/jpeg")
+
+            # os.remove("\\assets\\images\\"+filenameToBeSavedAs)
+
+
+
+
+
 
         return Response( {"status":"400" , "msg" : "bad request served"}, status=status.HTTP_400_BAD_REQUEST ,  content_type="image/jpeg")
